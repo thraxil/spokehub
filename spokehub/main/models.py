c6519027a76c077822f931c563919cc0bd4c2b91
Anders Pearson <anders@columbia.edu>
2014-02-16 12:03:36 -0500

    define main model

--- spokehub/main/models.py
+++ spokehub/main/models.py
@@ -1,3 +1,30 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# define your models here
+
+class Item(models.Model):
+    title = models.CharField(max_length=256)
+    section = models.CharField(
+        max_length=256,
+        choices=(('news', 'News Item'),
+                 ('challenge', 'Challenge'),
+                 ('case', 'Case Study')))
+    body = models.TextField(blank=True, default=u"")
+    added = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ['section', 'added']
+
+
+class Reply(models.Model):
+    item = models.ForeignKey(Item)
+    author = models.ForeignKey(User)
+    body = models.TextField(blank=True, default=u"")
+    added = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        order_with_respect_to = 'item'
+        ordering = ['added']
