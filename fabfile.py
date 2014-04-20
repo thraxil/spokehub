5aac81f0fa934aa0046a2cc1a4a495260e1ad8eb
Anders Pearson <anders@columbia.edu>
2014-04-20 10:38:11 -0400

    make a quicker deploy pipeline that doesn't bootstrap and migrate

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,14 @@ env.hosts = ['orlando.thraxil.org']
 env.user = 'anders'
 nginx_hosts = ['lolrus.thraxil.org']
 
+
 def restart_gunicorn():
     sudo("restart spokehub")
 
+
 def prepare_deploy():
     local("./manage.py test")
+
 
 def deploy():
     code_dir = "/var/www/spokehub/spokehub"
@@ -21,3 +24,13 @@ def deploy():
             run(("rsync -avp media/ "
                  "%s:/var/www/spokehub/spokehub/media/") % n)
     restart_gunicorn()
+
+
+def design_deploy():
+    code_dir = "/var/www/spokehub/spokehub"
+    with cd(code_dir):
+        run("git pull origin master")
+        run("./manage.py collectstatic --noinput --settings=spokehub.settings_production")
+        for n in nginx_hosts:
+            run(("rsync -avp media/ "
+                 "%s:/var/www/spokehub/spokehub/media/") % n)
