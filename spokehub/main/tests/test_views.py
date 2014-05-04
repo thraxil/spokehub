b9b31119039f2e5452d11230311399ef91f88b5f
Anders Pearson <anders@columbia.edu>
2014-05-03 21:20:09 -0400

    add a makefile for convenience

--- spokehub/main/tests/test_views.py
+++ spokehub/main/tests/test_views.py
@@ -13,4 +13,3 @@ class BasicTest(TestCase):
     def test_smoketest(self):
         response = self.c.get("/smoketest/")
         self.assertEquals(response.status_code, 200)
-        assert "PASS" in response.content
