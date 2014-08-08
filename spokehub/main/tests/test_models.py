ebf51d5d57b964bebf7a200080df2ff052cae4dd
Anders Pearson <anders@columbia.edu>
2014-08-08 12:16:06 +0200

    add a bit of test coverage

--- spokehub/main/tests/test_models.py
+++ spokehub/main/tests/test_models.py
@@ -1,6 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from spokehub.main.models import Item
 
 
-class BasicModelTest(TestCase):
-    def test_dummy(self):
-        assert True
+class ItemTest(TestCase):
+    def test_get_absolute_url(self):
+        u = User.objects.create(username='test')
+        i = Item.objects.create(author=u, title='foo', section='case')
+        self.assertTrue(i.get_absolute_url().startswith('/case/'))
+
+    def test_touch(self):
+        u = User.objects.create(username='test')
+        i = Item.objects.create(author=u, title='foo', section='case')
+        i.touch()
+
+    def test_add_reply(self):
+        u = User.objects.create(username='test')
+        i = Item.objects.create(author=u, title='foo', section='case')
+        i.add_reply(u, 'a body')
