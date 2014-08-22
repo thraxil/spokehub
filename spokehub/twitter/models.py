f4748aba6d12650e33b9a8159e8175c23c6f2936
Anders Pearson <anders@columbia.edu>
2014-08-22 22:46:58 +0200

    add fields for twitter screen name and profile image

--- spokehub/twitter/models.py
+++ spokehub/twitter/models.py
@@ -6,3 +6,5 @@ class TwitterAccount(models.Model):
     user = models.ForeignKey(User)
     oauth_token = models.TextField(blank=True, default='')
     oauth_verifier = models.TextField(blank=True, default='')
+    screen_name = models.TextField(blank=True, default='')
+    profile_image_url = models.TextField(blank=True, default='')
