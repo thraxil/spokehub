a3944c96146a6fbd6e22608ee0f4846a2687832d
Anders Pearson <anders@columbia.edu>
2014-08-22 23:01:12 +0200

    refactor out twitter_api call

--- spokehub/twitter/models.py
+++ spokehub/twitter/models.py
@@ -11,12 +11,15 @@ class TwitterAccount(models.Model):
     screen_name = models.TextField(blank=True, default='')
     profile_image_url = models.TextField(blank=True, default='')
 
-    def update_details(self):
-        """ fill in screen name and profile image """
+    def twitter_api(self):
         auth = tweepy.OAuthHandler(
             settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
         auth.set_access_token(self.oauth_token, self.oauth_verifier)
-        api = tweepy.API(auth)
+        return tweepy.API(auth)
+
+    def update_details(self):
+        """ fill in screen name and profile image """
+        api = self.twitter_api()
         tw_user = api.me()
         self.screen_name = tw_user.screen_name
         self.profile_image_url = tw_user.profile_image_url
