41466a9a99274b52028af1df018f3921d62989e3
Anders Pearson <anders@columbia.edu>
2015-01-31 13:33:12 +0100

    :white_check_mark: strengthened some tests

--- spokehub/main/tests/test_models.py
+++ spokehub/main/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .factories import UserFactory, ItemFactory
+from spokehub.main.models import Item
 
 
 class ItemTest(TestCase):
@@ -7,11 +8,19 @@ class ItemTest(TestCase):
         i = ItemFactory()
         self.assertTrue(i.get_absolute_url().startswith('/item/'))
 
+    def test_unicode(self):
+        i = ItemFactory()
+        self.assertTrue(str(i), "foo")
+
     def test_touch(self):
         i = ItemFactory()
+        modified = i.modified
         i.touch()
+        i2 = Item.objects.get(id=i.id)
+        self.assertTrue(i2.modified > modified)
 
     def test_add_reply(self):
         u = UserFactory()
         i = ItemFactory()
         i.add_reply(u, 'a body')
+        self.assertEqual(i.reply_set.all().count(), 1)
