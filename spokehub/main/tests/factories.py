91c62ec4e8d8db7780b7fe7dd7f583f668cb53a1
Patrick Krawczykowski <pkrawz@gmail.com>
2015-02-03 16:18:06 +0100

    Revert dcf8e5b..059f700
    This rolls back to commit dcf8e5b37628c8dd09f8750b39dc030bbfbe2540.

--- spokehub/main/tests/factories.py
+++ spokehub/main/tests/factories.py
@@ -1,23 +1,15 @@
 import factory
 
 from django.contrib.auth.models import User
-from spokehub.main.models import Item, Reply
+from spokehub.main.models import Item
 
 
 class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = User
-    username = factory.Sequence(lambda n: 'user{0}'.format(n))
-    email = factory.Sequence(lambda n: 'user{0}@example.com'.format(n))
+    username = "testuser"
+    email = "test@example.com"
 
 
 class ItemFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Item
     title = 'foo'
-
-
-class ReplyFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = Reply
-    item = factory.SubFactory(ItemFactory)
-    author = factory.SubFactory(UserFactory)
-    body = "reply body"
-    title = "reply title"
