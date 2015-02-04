f7825b1ebb2537775d3049f6a300f793c4a9e5b0
Patrick Krawczykowski <pkrawz@gmail.com>
2015-02-04 13:48:09 +0100

    change

--- spokehub/main/tests/test_views.py
+++ spokehub/main/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.client import Client
+from waffle.models import Flag
 from .factories import (UserFactory, ItemFactory)
 
 
@@ -46,6 +47,7 @@ class LoggedInTest(TestCase):
         self.assertTrue('foo' in r.content)
 
     def test_reploy_to_item(self):
+        Flag.objects.create(name="main", everyone=True)
         i = ItemFactory()
         r = self.c.post(
             "/item/%d/reply/" % i.id,
