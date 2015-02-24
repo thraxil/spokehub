8e59dce53c81f32ac0c285cb29467fa01e399072
Patrick Krawczykowski <pkrawz@gmail.com>
2015-02-24 17:59:41 +0100

    basic animation function

--- sandbox/app/scripts/main.js
+++ sandbox/app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function(){
+  $('.section-link').click(function(){
+    var target = $(this).closest('section');
+    console.log(target);
+    if (target.hasClass('active')) {
+      target.removeClass('active');
+    } else {
+      target.addClass('active');
+    }
+  });
+});
