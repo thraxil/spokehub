59475d05314d3354136a3a51fd87163f519e70a2
Patrick Krawczykowski <pkrawz@gmail.com>
2015-02-25 16:18:33 +0100

    js animation mostly done

--- sandbox/app/scripts/main.js
+++ sandbox/app/scripts/main.js
@@ -1,13 +1,37 @@
 'use strict';
 
 $(document).ready(function(){
-  $('.section-link').click(function(){
-    var target = $(this).closest('section');
-    console.log(target);
-    if (target.hasClass('active')) {
-      target.removeClass('active');
-    } else {
-      target.addClass('active');
-    }
+  $('.column').click(function(){
+
+    var target = $(this);
+    var others = $(this).siblings();
+    var sectionTarget = target.attr('id');
+
+    target.addClass('expanded');
+
+    others.removeClass('expanded');
+
+    $('.column').each(function(){
+      if ($(this).hasClass('full-column')){
+        $(this).removeClass('full-column');
+        $(this).addClass('tabed-column');
+      }
+    });
+
+    $('.main-section').each(function(){
+      if ($(this).data('target') === sectionTarget) {
+        $(this).addClass('show');
+      } else {
+        $(this).removeClass('show').addClass('hide');
+
+      }
+    });
+  });
+  $('#header-wrapper h2').click(function(){
+    $('.main-section').each(function(){
+      $(this).addClass('hide');
+      $(this).removeClass('show');
+    });
+    $('.column').addClass('full-column').removeClass('tabed-column').removeClass('expanded');
   });
 });
