7df69dd9a35bb9a5c5219397cdc50981fc887260
Anders Pearson <anders@columbia.edu>
2015-03-15 23:12:43 +0100

    need compress in both

--- fabfile.py
+++ fabfile.py
@@ -19,6 +19,7 @@ def deploy():
         run("git pull origin master")
         run("make migrate")
         run("make collectstatic")
+        run("make compress")
         for n in nginx_hosts:
             run(("rsync -avp media/ "
                  "%s:/var/www/spokehub/spokehub/media/") % n)
