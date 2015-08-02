fbf5bf12068a6a414a7e6c02690c182d45191299
Anders Pearson <anders@columbia.edu>
2015-08-02 22:35:58 +0200

    remove unused templatetag

--- spokehub/main/templatetags/urlize_target_blank.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-def url_target_blank(text):
-    return text.replace('<a ', '<a target="_blank" ')
-url_target_blank = register.filter(url_target_blank)
-url_target_blank.is_safe = True
