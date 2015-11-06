39053ad817ae9a6a802ade272b36039a1c97b0d4
Anders Pearson <anders@columbia.edu>
2015-11-06 14:06:47 +0000

    :arrow_up: Django 1.8.6

--- spokehub/invite/migrations/0001_initial.py
+++ spokehub/invite/migrations/0001_initial.py
@@ -1,41 +1,25 @@
 # flake8: noqa
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Invite'
-        db.create_table(u'invite_invite', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('email', self.gf('django.db.models.fields.TextField')()),
-            ('token', self.gf('django.db.models.fields.TextField')()),
-            ('status', self.gf('django.db.models.fields.TextField')(default='OPEN')),
-            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal(u'invite', ['Invite'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Invite'
-        db.delete_table(u'invite_invite')
-
-
-    models = {
-        u'invite.invite': {
-            'Meta': {'object_name': 'Invite'},
-            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'email': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'status': ('django.db.models.fields.TextField', [], {'default': "'OPEN'"}),
-            'token': ('django.db.models.fields.TextField', [], {})
-        }
-    }
-
-    complete_apps = ['invite']
+    operations = [
+        migrations.CreateModel(
+            name='Invite',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.TextField()),
+                ('token', models.TextField()),
+                ('status', models.TextField(default=b'OPEN')),
+                ('added', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
