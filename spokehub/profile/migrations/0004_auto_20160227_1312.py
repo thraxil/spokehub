cfcb6314d253856224476a71129ed686a80dc1d9
Anders Pearson <anders@columbia.edu>
2016-02-27 13:15:05 +0000

    data migration to merge city and country into location. #79

---
+++ spokehub/profile/migrations/0004_auto_20160227_1312.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def merge_to_location(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+    for u in User.objects.all():
+        u.profile.location = "%s, %s" % (u.profile.city, u.profile.country)
+        u.profile.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profile', '0003_profile_location'),
+    ]
+
+    operations = [
+        migrations.RunPython(merge_to_location),
+    ]
