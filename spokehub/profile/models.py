dca31e8a32fb3555635fc7eeed5a4574ee907ba1
Anders Pearson <anders@columbia.edu>
2016-02-28 16:12:22 +0000

    add cover photo to profile. #76

--- spokehub/profile/models.py
+++ spokehub/profile/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
+from easy_thumbnails.fields import ThumbnailerImageField
 
 
 class Profile(UserenaBaseProfile):
@@ -15,6 +16,7 @@ class Profile(UserenaBaseProfile):
     website_url = models.CharField(max_length=256, blank=True, default="")
     website_name = models.CharField(max_length=256, blank=True, default="")
     location = models.CharField(max_length=256, blank=True, default="")
+    cover = ThumbnailerImageField('cover', blank=True, upload_to='covers')
 
     def completed(self):
         return (
