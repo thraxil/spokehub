7911114b1896ea0dbaa0c0dba4f45f11fb525cce
Anders Pearson <anders@columbia.edu>
2016-03-03 18:24:43 +0000

    hack to get instagram sorted
    * patched instagram library to fix a KeyError bug
    * use two different client accounts to access account vs hashtag

--- spokehub/main/management/commands/update_now.py
+++ spokehub/main/management/commands/update_now.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from spokehub.twitter import hashtag_search as twitter_hashtag_search
 from spokehub.twitter import my_tweets
 from spokehub.instagram import hashtag_search as instagram_hashtag_search
+from spokehub.instagram import my_posts
 from spokehub.tumblr import hashtag_search as tumblr_hashtag_search
 
 
@@ -13,4 +14,5 @@ class Command(BaseCommand):
         my_tweets()
         tumblr_hashtag_search()
         twitter_hashtag_search()
+        my_posts()
         instagram_hashtag_search()
