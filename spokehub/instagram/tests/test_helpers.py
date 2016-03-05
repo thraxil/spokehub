c6071c031fa91376b5c0c5759325b6d1a5260c20
Anders Pearson <anders@columbia.edu>
2016-03-05 20:30:41 +0000

    cover my_posts and hashtag_search

--- spokehub/instagram/tests/test_helpers.py
+++ spokehub/instagram/tests/test_helpers.py
@@ -1,9 +1,20 @@
 import unittest
-from spokehub.instagram import image_image_url, add_post, add_media
+from spokehub.instagram import (
+    image_image_url, add_post, add_media, hashtag_search,
+    my_posts,
+)
 
 
 class Dummy(object):
     pass
+
+
+class DummyAPI(object):
+    def user_recent_media(self):
+        return ([], None)
+
+    def tag_recent_media(self, **kwargs):
+        return ([], None)
 
 
 class TestImageURL(unittest.TestCase):
@@ -30,3 +41,15 @@ class TestAddPosts(unittest.TestCase):
         d = Dummy()
         d.link = None
         self.assertIsNone(add_media([d]))
+
+
+class TestHashTagSearch(unittest.TestCase):
+    def test_hashtag_search(self):
+        api = DummyAPI()
+        self.assertIsNone(hashtag_search(api))
+
+
+class TestMyPosts(unittest.TestCase):
+    def test_my_posts(self):
+        api = DummyAPI()
+        self.assertIsNone(my_posts(api))
