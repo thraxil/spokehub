74effad4a7c1a63323d212e43a1a12e7baefa362
Anders Pearson <anders@columbia.edu>
2016-03-10 19:52:05 +0000

    put a maxlength=140 on reply/convo edit forms

--- spokehub/main/forms.py
+++ spokehub/main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Reply
+from .models import Reply, Conversation
 
 
 class ReplyUpdateForm(forms.ModelForm):
@@ -8,4 +8,14 @@ class ReplyUpdateForm(forms.ModelForm):
         fields = ['image', 'url', 'body']
         widgets = {
             'url': forms.TextInput(attrs=dict(maxlength=140)),
+            'body': forms.Textarea(attrs=dict(maxlength=140)),
+        }
+
+
+class ConversationUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Conversation
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs=dict(maxlength=140)),
         }
