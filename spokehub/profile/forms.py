940c146a98d02872673b40966c4167c4b2076667
Anders Pearson <anders@columbia.edu>
2016-03-10 22:09:48 +0000

    get those down to one line each

--- spokehub/profile/forms.py
+++ spokehub/profile/forms.py
@@ -1,11 +1,7 @@
-from django import forms
+from django.forms import CharField
 from userena.forms import EditProfileForm
 
 
 class ExtendedEditProfileForm(EditProfileForm):
-    first_name = forms.CharField(label='First name',
-                                 max_length=30,
-                                 required=True)
-    last_name = forms.CharField(label='Last name',
-                                max_length=30,
-                                required=True)
+    first_name = CharField(label='First name', max_length=30, required=True)
+    last_name = CharField(label='Last name', max_length=30, required=True)
