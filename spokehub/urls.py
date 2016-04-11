bfa751b51f4239ff8ec8fcae54d88b2f09407dbc
Ariel Cotton <ariel@electricstud.io>
2016-04-11 09:01:29 +0200

    started on user profile index

--- spokehub/urls.py
+++ spokehub/urls.py
@@ -78,6 +78,8 @@ urlpatterns = patterns(
         template_name='main/conversation.html')),
     (r'^404-test/$', views.IndexView.as_view(
         template_name='404.html')),
+    url(r'^user_index/$', views.TemplateView.as_view(
+        template_name='userena/user_index.html')),
 
     url(r'^conversation/(?P<pk>\d+)/reply/$',
         views.ReplyToConversationView.as_view(),
