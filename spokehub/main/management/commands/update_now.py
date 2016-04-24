0e3645154d6a96b78164e609c9c4e80cef8b6be4
Anders Pearson <anders@columbia.edu>
2016-04-24 20:56:07 +0100

    pass in api to twitter

--- spokehub/main/management/commands/update_now.py
+++ spokehub/main/management/commands/update_now.py
@@ -7,6 +7,7 @@ from spokehub.tumblr import hashtag_search as tumblr_hashtag_search
 from instagram.client import InstagramAPI
 from django.conf import settings
 import pytumblr
+import tweepy
 
 
 class Command(BaseCommand):
@@ -14,7 +15,19 @@ class Command(BaseCommand):
     help = ''
 
     def handle(self, *args, **kwargs):
-        my_tweets()
+        CONSUMER_KEY = settings.TWITTER_API_KEY
+        CONSUMER_SECRET = settings.TWITTER_API_SECRET
+        ACCESS_KEY = settings.TWITTER_OAUTH_TOKEN
+        ACCESS_SECRET = settings.TWITTER_OAUTH_VERIFIER
+        USER = settings.TWITTER_USER
+
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        api = tweepy.API(auth)
+
+        my_tweets(api, USER)
+        twitter_hashtag_search(api, settings.HASHTAG)
+
         tumblr_client = pytumblr.TumblrRestClient(
             settings.TUMBLR_CONSUMER_KEY,
             settings.TUMBLR_CONSUMER_SECRET,
@@ -22,7 +35,7 @@ class Command(BaseCommand):
             settings.TUMBLR_OAUTH_SECRET,
         )
         tumblr_hashtag_search(tumblr_client)
-        twitter_hashtag_search()
+
         my_posts(InstagramAPI(access_token=settings.SH_INSTAGRAM_ACCESS_TOKEN))
         instagram_hashtag_search(
             InstagramAPI(access_token=settings.TH_INSTAGRAM_ACCESS_TOKEN))
