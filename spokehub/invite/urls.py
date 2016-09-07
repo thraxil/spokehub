c9d4f99b4fab2ff6400f69e8cfac220d08692531
Anders Pearson <anders@columbia.edu>
2016-09-07 20:42:32 +0100

    view pending invites.
    fixes #210

--- spokehub/invite/urls.py
+++ spokehub/invite/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Invite
 from .views import InviteView, SignupView
 
 
 urlpatterns = [
     url(r'^$', InviteView.as_view(), name='invite_form'),
+    url(r'list/$', ListView.as_view(model=Invite), name='invite-list'),
+    url(r'(?P<pk>\d+)/$', DetailView.as_view(model=Invite),
+        name='invite-detail'),
     url(r'^signup/(?P<token>[^/]+)/$',
         SignupView.as_view(), name='invite_signup_form'),
 ]
