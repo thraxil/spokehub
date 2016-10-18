6489343286ba8538b350583b2915c062c64335e2
Anders Pearson <anders@columbia.edu>
2016-10-18 21:34:23 +0100

    make hover_link() inclusion tag
    further simplify linking to profiles from templates

--- spokehub/main/templatetags/user_profile.py
+++ spokehub/main/templatetags/user_profile.py
@@ -8,9 +8,6 @@ class DummyUser(object):
     def __init__(self):
         self.dummy = True
 
-    def fullname(self):
-        return "dummy user"
-
 
 @register.simple_tag
 def user_profile(username):
@@ -20,3 +17,8 @@ def user_profile(username):
         return u
     except User.DoesNotExist:
         return DummyUser()
+
+
+@register.inclusion_tag('userena/hover_link.html')
+def hover_link(username, fullname):
+    return dict(username=username, fullname=fullname)
