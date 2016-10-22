51793399a72532e6d38a080959adf7c3ade2382f
Anders Pearson <anders@columbia.edu>
2016-10-22 11:53:54 +0100

    simplest possible list of projects
    closes #243

--- spokehub/work/urls.py
+++ spokehub/work/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import Project
 
 
 urlpatterns = [
+    url(r'^$', ListView.as_view(model=Project), name='project-list'),
     url(r'(?P<slug>\w+)/$', DetailView.as_view(model=Project),
         name='project-detail'),
 ]
