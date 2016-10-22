bf249947cad406822a89f778e4bbdfe3f87e5fb5
Anders Pearson <anders@columbia.edu>
2016-10-22 14:57:19 +0100

    pull out widgets_map

--- spokehub/work/forms.py
+++ spokehub/work/forms.py
@@ -3,26 +3,23 @@ from django.forms import ModelForm, TextInput
 from .models import Project
 
 
+widgets_map = {
+    'slug': TextInput(),
+    'title': TextInput(),
+    'subhead': TextInput(),
+    'date': TextInput(),
+}
+
+
 class EditProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'subhead', 'date', 'description']
-
-        widgets = {
-            'title': TextInput(),
-            'subhead': TextInput(),
-            'date': TextInput(),
-        }
+        widgets = widgets_map
 
 
 class CreateProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['slug', 'title', 'subhead', 'date', 'description']
-
-        widgets = {
-            'slug': TextInput(),
-            'title': TextInput(),
-            'subhead': TextInput(),
-            'date': TextInput(),
-        }
+        widgets = widgets_map
