4e82af5f6c0dd9f7820bdbb727ebde8cae6d543e
Anders Pearson <anders@thraxil.org>
2017-12-09 21:17:26 +0000

    pull base model in from userena

--- spokehub/profile/models.py
+++ spokehub/profile/models.py
@@ -1,9 +1,150 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.template.loader import get_template
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext as _
-from userena.models import UserenaBaseProfile
+from guardian.shortcuts import get_perms
 from easy_thumbnails.fields import ThumbnailerImageField
+from userena.managers import UserenaBaseProfileManager
+from userena.utils import (get_gravatar, generate_sha1, get_datetime_now)
+
+
+USERENA_MUGSHOT_SIZE = getattr(settings,
+                               'USERENA_MUGSHOT_SIZE',
+                               80)
+USERENA_MUGSHOT_CROP_TYPE = getattr(settings,
+                                    'USERENA_MUGSHOT_CROP_TYPE',
+                                    'smart')
+USERENA_WITHOUT_USERNAMES = getattr(settings,
+                                    'USERENA_WITHOUT_USERNAMES',
+                                    False)
+USERENA_MUGSHOT_GRAVATAR = getattr(settings,
+                                   'USERENA_MUGSHOT_GRAVATAR',
+                                   True)
+USERENA_MUGSHOT_DEFAULT = getattr(settings,
+                                  'USERENA_MUGSHOT_DEFAULT',
+                                  'identicon')
+USERENA_DEFAULT_PRIVACY = getattr(settings,
+                                  'USERENA_DEFAULT_PRIVACY',
+                                  'registered')
+USERENA_MUGSHOT_PATH = getattr(settings,
+                               'USERENA_MUGSHOT_PATH',
+                               'mugshots/')
+PROFILE_PERMISSIONS = (
+            ('view_profile', 'Can view profile'),
+)
+
+
+def upload_to_mugshot(instance, filename):
+    """
+    Uploads a mugshot for a user to the ``USERENA_MUGSHOT_PATH`` and
+    saving it under unique hash for the image. This is for privacy
+    reasons so others can't just browse through the mugshot directory.
+
+    """
+    extension = filename.split('.')[-1].lower()
+    salt, hash = generate_sha1(instance.pk)
+    path = USERENA_MUGSHOT_PATH % {
+        'username': instance.user.username,
+        'id': instance.user.id,
+        'date': instance.user.date_joined,
+        'date_now': get_datetime_now().date()}
+    return '%(path)s%(hash)s.%(extension)s' % {'path': path,
+                                               'hash': hash[:10],
+                                               'extension': extension}
+
+
+@python_2_unicode_compatible
+class UserenaBaseProfile(models.Model):
+    """ Base model needed for extra profile functionality """
+    PRIVACY_CHOICES = (
+        ('open', _('Open')),
+        ('registered', _('Registered')),
+        ('closed', _('Closed')),
+    )
+
+    MUGSHOT_SETTINGS = {'size': (USERENA_MUGSHOT_SIZE,
+                                 USERENA_MUGSHOT_SIZE),
+                        'crop': USERENA_MUGSHOT_CROP_TYPE}
+
+    mugshot = ThumbnailerImageField(
+        _('mugshot'),
+        blank=True,
+        upload_to=upload_to_mugshot,
+        resize_source=MUGSHOT_SETTINGS,
+        help_text=_('A personal image displayed in your profile.'))
+
+    privacy = models.CharField(
+        _('privacy'),
+        max_length=15,
+        choices=PRIVACY_CHOICES,
+        default=USERENA_DEFAULT_PRIVACY,
+        help_text=_('Designates who can view your profile.'))
+
+    objects = UserenaBaseProfileManager()
+
+    class Meta:
+        abstract = True
+        permissions = PROFILE_PERMISSIONS
+
+    def __str__(self):
+        return 'Profile of %(username)s' % {'username': self.user.username}
+
+    def get_mugshot_url(self):
+        # First check for a mugshot and if any return that.
+        if self.mugshot:
+            return self.mugshot.url
+
+        # Use Gravatar if the user wants to.
+        if USERENA_MUGSHOT_GRAVATAR:
+            return get_gravatar(self.user.email,
+                                USERENA_MUGSHOT_SIZE,
+                                USERENA_MUGSHOT_DEFAULT)
+
+        # Gravatar not used, check for a default image.
+        else:
+            if USERENA_MUGSHOT_DEFAULT not in ['404', 'mm',
+                                               'identicon',
+                                               'monsterid',
+                                               'wavatar']:
+                return USERENA_MUGSHOT_DEFAULT
+            else:
+                return None
+
+    def get_full_name_or_username(self):
+        user = self.user
+        if user.first_name or user.last_name:
+            # We will return this as translated string. Maybe there are some
+            # countries that first display the last name.
+            name = _("%(first_name)s %(last_name)s") % \
+                {'first_name': user.first_name,
+                 'last_name': user.last_name}
+        else:
+            # Fallback to the username if usernames are used
+            if not USERENA_WITHOUT_USERNAMES:
+                name = "%(username)s" % {'username': user.username}
+            else:
+                name = "%(email)s" % {'email': user.email}
+        return name.strip()
+
+    def can_view_profile(self, user):
+        # Simple cases first, we don't want to waste CPU and DB hits.
+        # Everyone.
+        if self.privacy == 'open':
+            return True
+        # Registered users.
+        elif self.privacy == 'registered' and isinstance(
+                user, get_user_model()):
+            return True
+
+        # Checks done by guardian for owner and admins.
+        elif 'view_profile' in get_perms(user, self):
+            return True
+
+        # Fallback to closed profile.
+        return False
 
 
 class Profile(UserenaBaseProfile):
