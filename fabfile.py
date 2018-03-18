a9c1eba618179db1f5453bf504305f231b3c0e87
Anders Pearson <anders@thraxil.org>
2018-03-18 22:40:38 +0000

    quoting

--- fabfile.py
+++ fabfile.py
@@ -29,9 +29,9 @@ def sentry():
            "305941/a1598af8c2efee2a4fd205aff2386cad5b25e"
            "451c31f2fbfc89b5f85b8d0232b/")
     local("""COMMIT=$(git log -n 1 --pretty=format:'%%H') && curl %s \
-  -X POST \
-  -H 'Content-Type: application/json' \
-    -d '{"version": "$COMMIT"}'""" % (url))
+    -X POST \
+    -H 'Content-Type: application/json' \
+    -d "{\"version\": \"$COMMIT\"}" """ % (url))
 
 
 def deploy():
